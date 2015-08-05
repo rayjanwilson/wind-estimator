3725bd14db66c4f0e9682d7d7e6262a0b50d493f
Rayjan Wilson <rayjan.wilson@gmail.com>
2015-08-05 01:58:25 -0800

    it now samples the log every gps second for any variable of interest

--- run.py
+++ run.py
@@ -1,83 +1,71 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import division
-import DataFlash as DataflashLog
+from DataflashLog import DataflashLog, LogIterator, DataflashLogHelper
 import collections
 
-logfile = "logs/15-07-17_11-29-32.log"
-logdata = DataflashLog.DataflashLog()
+#logfile = "logs/15-07-17_11-29-32.log"
+logfile = "logs/15-07-17_11-57-40.log"
 print("Parsing: ", logfile)
-logdata.read(logfile, ignoreBadlines=True)
+logdata = DataflashLog(logfile)
 print("Complete")
 
-print("GPS Labels: ", logdata.formats['GPS'].labels)
-print("ATT Labels: ", logdata.formats['ATT'].labels)
-print("EKF1 Labels: ", logdata.formats['EKF1'].labels)
+#print("GPS Labels: ", logdata.formats['GPS'].labels)
+#print("ATT Labels: ", logdata.formats['ATT'].labels)
+#print("EKF1 Labels: ", logdata.formats['EKF1'].labels)
 print("Duration [s]: ", logdata.durationSecs)
 
-lit = DataflashLog.LogIterator(logdata)
-#print("lit current line: ", lit.currentLine)
-#print("lit iterators: ", lit.iterators)
+loiterChunks = DataflashLogHelper.findLoiterChunks(logdata)
+print("Number of loiters: ", len(loiterChunks))
+print(loiterChunks)
 
-# figure out where the Loiter modes begin and end
-# https://github.com/diydrones/ardupilot/blob/master/Tools/LogAnalyzer/tests/TestPitchRollCoupling.py#L29
-autoModes   = ["RTL","AUTO","LAND","LOITER","GUIDED","CIRCLE","OF_LOITER","HYBRID"]     # use NTUN DRol+DPit
-manualModes = ["STABILIZE","DRIFT","ALTHOLD","ALT_HOLD","POSHOLD"]                      # use CTUN RollIn/DesRoll + PitchIn/DesPitch
-ignoreModes = ["ACRO","SPORT","FLIP","AUTOTUNE",""]                            # ignore data from these modes
-autoSegments   = []  # list of (startLine,endLine) pairs
-manualSegments = []  # list of (startLine,endLine) pairs
-orderedModes = collections.OrderedDict(sorted(logdata.modeChanges.items(), key=lambda t: t[0]))
-isAuto = False # we always start in a manual control mode
-prevLine = 0
-mode = ""
-for line,modepair in orderedModes.iteritems():
-    mode = modepair[0].upper()
-    if prevLine == 0:
-        prevLine = line
-    if mode in autoModes:
-        if not isAuto:
-            manualSegments.append((prevLine,line-1))
-            prevLine = line
-        isAuto = True
-    elif mode in manualModes:
-        if isAuto:
-            autoSegments.append((prevLine,line-1))
-            prevLine = line
-        isAuto = False
-    elif mode in ignoreModes:
-        if isAuto:
-            autoSegments.append((prevLine,line-1))
-        else:
-            manualSegments.append((prevLine,line-1))
-        prevLine = 0
-    else:
-        raise Exception("Unknown mode in TestPitchRollCoupling: %s" % mode)
 
-# and handle the last segment, which doesn't have an ending
-if mode in autoModes:
-    autoSegments.append((prevLine,logdata.lineCount))
-elif mode in manualModes:
-    manualSegments.append((prevLine,logdata.lineCount))
+# ignore anything below this altitude, to discard any data while not flying
+minAltThreshold = 1.5 # meters
 
-print(len(autoSegments))
-print(autoSegments)
+for (startLine,endLine) in loiterChunks:
+    lit = LogIterator(logdata, startLine)
+    assert(lit.currentLine == startLine)
+    gpsStart = lit["GPS"]["GMS"]
+    increment = 1000
+    nextgpsTime = gpsStart + increment
+    print("GPS Start: ", gpsStart)
+    i=0
+    while lit.currentLine <= endLine:
+        if (nextgpsTime <= lit["GPS"]["GMS"]):
+            # only parse the log every gps second
+            timeUS = lit["GPS"]["TimeUS"]
+            timeGPS = lit["GPS"]["GMS"]
+            roll  = lit["EKF1"]["Roll"]
+            pitch = lit["EKF1"]["Pitch"]
+            yaw = lit["EKF1"]["Yaw"]
+            vd = lit["EKF1"]["VD"]
+            pd = lit["EKF1"]["PD"]
+            lat = lit["GPS"]["Lat"]
+            lon = lit["GPS"]["Lng"]
 
-# gps = logdata.channels["GPS"]
-# gps_timeus = gps["TimeUS"].listData
-# gps_lat = gps["Lat"].listData
+            print(i, lit.currentLine, timeUS, timeGPS, roll, pitch, yaw, vd, pd, lat, lon)
+
+            nextgpsTime = nextgpsTime + increment
+            i += 1
+        lit.next()
+
+# really want to loop through the log by gps seconds
+# either record the line number, or do a snapshot when the gps has reached the next second
+# and then do a snapshot for each
 #
-# att = logdata.channels["ATT"]
-# att_timeus = att["TimeUS"].listData
-# att_roll = att["Roll"].listData
-#
-# gps1 = []
-# att1 = []
-#
-# for i in range(len(gps_timeus)):
-#     gps1.append({ 't': gps_timeus[i][1], 'lat': gps_lat[i][1] })
-#
-# for i in range(len(att_timeus)):
-#     att1.append({ 't': att_timeus[i][1], 'roll': att_roll[i][1] })
-#
-# gps1.sort(key=lambda x: x['t'])
-# att1.sort(key=lambda x: x['t'])
+
+# hovering = (lit["EKF1"]["VD"] < 0.2) and (lit["EKF1"]["VE"] < 0.2) and (lit["EKF1"]["VN"] < 0.2)
+# hovering = (lit["EKF1"]["VD"] > -0.2) and (lit["EKF1"]["VE"] > -0.2) and (lit["EKF1"]["VN"] > -0.2) and hovering
+# goodGPS = (lit["GPS"]["Status"] == 3)
+# aboveMinHeight = (lit["EKF1"]["PD"] < -1*minAltThreshold)
+#if (goodGPS and hovering and aboveMinHeight):
+# timeUS = lit["GPS"]["TimeUS"]
+# timeGPS = lit["GPS"]["GMS"]
+# roll  = lit["EKF1"]["Roll"]
+# pitch = lit["EKF1"]["Pitch"]
+# yaw = lit["EKF1"]["Yaw"]
+# vd = lit["EKF1"]["VD"]
+# pd = lit["EKF1"]["PD"]
+# lat = lit["GPS"]["Lat"]
+# lon = lit["GPS"]["Lng"]
