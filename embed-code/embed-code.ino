ff21d38487edaeaf1d512751c1bbe1d65907605b
Rayjan Wilson <rayjan.wilson@gmail.com>
2015-08-06 09:28:09 -0800

    sd card is now working, had to drop the gps

--- embed-code/embed-code.ino
+++ embed-code/embed-code.ino
@@ -1,102 +1,54 @@
 #include <Wire.h>
 #include "RTClib.h"
-#include <Adafruit_GPS.h>
-#include <SoftwareSerial.h>
 #include "Adafruit_TMP007.h"
 #include "Adafruit_HTU21DF.h"
+#include <SPI.h>
+#include "SD.h"
+
+// Set the pins used
+#define chipSelect 10
+
+// the logging file
+File logfile;
+char filename[8];
 
 RTC_DS1307 rtc;
 Adafruit_TMP007 tmp007;
 Adafruit_HTU21DF htu = Adafruit_HTU21DF();
 
-SoftwareSerial mySerial(3, 2);
-Adafruit_GPS GPS(&mySerial);
-
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
-#define GPSECHO  false
-
-// this keeps track of whether we're using the interrupt
-// off by default!
-boolean usingInterrupt = false;
-void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
-
-
 void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-  rtc.begin();
+  Serial.begin(57600);
 
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+  Serial.println("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
   
-  if (! tmp007.begin(TMP007_CFG_4SAMPLE)) {
-    Serial.println("No sensor found");
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("Card initialized");
+  
+  Wire.begin();
+  if(!rtc.begin()){
+    Serial.println("RTC failed");
+  }
+  DateTime now = rtc.now();
+  //char buf[50];
+  sprintf(filename, "%02d%02d%02d%02d", now.month(), now.day(), now.hour(), now.minute());
+  Serial.print("logging to "); Serial.println(filename);
+  
+  if (! tmp007.begin((uint8_t)TMP007_CFG_4SAMPLE)) {
+    Serial.println("No temperature sensor found");
     while (1);
   }
   
   if (!htu.begin()) {
-    Serial.println("Couldn't find sensor!");
+    Serial.println("Couldnt find humidity sensor!");
     while (1);
-  }
-  
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
-  
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  // the nice thing about this code is you can have a timer0 interrupt go off
-  // every 1 millisecond, and read data from the GPS for you. that makes the
-  // loop code a heck of a lot easier!
-  useInterrupt(true);
-
-  delay(1000);
-  // Ask for firmware version
-  mySerial.println(PMTK_Q_RELEASE);
-}
-
-// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-#ifdef UDR0
-  if (GPSECHO)
-    if (c) UDR0 = c;  
-    // writing direct to UDR0 is much much faster than Serial.print 
-    // but only one character can be written at a time. 
-#endif
-}
-
-void useInterrupt(boolean v) {
-  if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-    usingInterrupt = true;
-  } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
-    usingInterrupt = false;
   }
 }
 
@@ -104,64 +56,46 @@ uint32_t timer = millis();
 
 void loop() {
 
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  // in case you are not using the interrupt above, you'll
-  // need to 'hand query' the GPS, not suggested :(
-  if (! usingInterrupt) {
-    // read data from the GPS in the 'main loop'
-    char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO)
-      if (c) Serial.print(c);
-  }
-  
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences! 
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-  
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
-  }
-
   // if millis() or timer wraps around, we'll just reset it
   if (timer > millis())  timer = millis();
 
   // approximately every 1 seconds or so, print out the current stats
   if (millis() - timer > 1000) { 
     timer = millis(); // reset the timer
-    DateTime now = rtc.now();
-    float objt = tmp007.readObjTempC();
-    float diet = tmp007.readDieTempC();
 
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    Serial.println(" ");
-    if (GPS.fix) {
-      if(GPS.seconds != now.second()){ 
-        rtc.adjust(DateTime(2000+GPS.year, GPS.month, GPS.day, GPS.hour, GPS.minute, GPS.seconds));
+
+      float objt = tmp007.readObjTempC();
+      float diet = tmp007.readDieTempC();
+      float hum = htu.readHumidity();
+      
+      DateTime now = rtc.now();
+      //long seconds = now.secondstime();
+      
+      File logfile = SD.open(filename, FILE_WRITE);
+      if (logfile) {
+        logfile.print(now.unixtime());
+        logfile.print(", ");
+        logfile.print(objt);
+        logfile.print(", ");
+        logfile.print(diet);
+        logfile.print(", ");
+        logfile.print(hum);
+        logfile.println();
+        logfile.close();
+        
+      }else{
+        Serial.println("error writing data to data.txt");
       }
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", "); 
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Location (in degrees, works with Google Maps): ");
-      Serial.print(GPS.latitudeDegrees, 4);
-      Serial.print(", "); 
-      Serial.println(GPS.longitudeDegrees, 4);
-      
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Object Temperature: "); Serial.print(objt); Serial.println("*C");
-      Serial.print("Die Temperature: "); Serial.print(diet); Serial.println("*C");
-      Serial.print("Hum: "); Serial.println(htu.readHumidity());
-      
+      Serial.print(now.unixtime());
+      Serial.print(", ");
+      Serial.print(objt);
+      Serial.print(", ");
+      Serial.print(diet);
+      Serial.print(", ");
+      Serial.print(hum);      
       Serial.println(" ");
-    }
+
   }
+
+  
 }
