49ef2f4e645c03948c43872b513983b2cbfed984
Rayjan Wilson <rayjan.wilson@gmail.com>
2015-08-09 20:34:50 -0800

    proper tilt angle calculation

--- estimate.py
+++ estimate.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
+from __future__ import division
 import numpy as np
+import numpy.linalg as la
 from math import sin, cos, tan, pi
+
+def ang(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'    """
+    cosang = np.dot(v1, v2)
+    sinang = la.norm(np.cross(v1, v2))
+    return np.arctan2(sinang, cosang)
 
 def mdot(*args):
     return reduce(np.dot, args)
@@ -30,7 +38,7 @@ def estimate_wind(yaw=0, pitch=0, roll=0, compass=0):
     Rcom = np.array([
     [cos(Compass), sin(Compass), 0],
     [-sin(Compass), cos(Compass), 0],
-    [0, 0, -1]
+    [0, 0, 1]
     ])
 
     # Ryaw_t = np.transpose(Ryaw)
@@ -38,15 +46,21 @@ def estimate_wind(yaw=0, pitch=0, roll=0, compass=0):
     # Rroll_t = np.transpose(Rroll)
     # Rcom_t = np.transpose(Rcom)
 
-    IJK = mdot(Ryaw, Rpitch, Rroll)
-    ENU = mdot(Rcom, IJK)
+    ijk = mdot(Ryaw, Rpitch, Rroll) # in terms of IJK
+    print("ijk:\n{}").format(ijk)
+    ijkENU = mdot(Rcom, ijk) # in terms of ENU
+    print("ijkENU\n{}").format(ijkENU)
 
-    E = mdot(np.array([1, 0, 0]), ENU)
-    N = mdot(np.array([0, 1, 0]), ENU)
-    U = mdot(np.array([0, 0, 1]), ENU)
+    iENU = mdot(np.array([1, 0, 0]), ijkENU)
+    jENU = mdot(np.array([0, 1, 0]), ijkENU)
+    kENU = mdot(np.array([0, 0, 1]), ijkENU)
 
+    k_unit = np.linalg.norm(kENU)
+    print("kENU {}").format(kENU)
+    U = np.array([0, 0, 1])
 
-    print("{}\n{}\n{}\n").format(E, N, U)
+    tiltAngle = ang(kENU, U) * 180/pi
+    print("tilt: {}\n\n").format(tiltAngle)
 
 
 if __name__ == "__main__":
@@ -63,3 +77,5 @@ if __name__ == "__main__":
     estimate_wind(45, pitch, roll, 45)
     print("compass small angle")
     estimate_wind(yaw, pitch, roll, 1)
+    print("compass 0, pitch 45")
+    estimate_wind(0, 45, 0, 0)
